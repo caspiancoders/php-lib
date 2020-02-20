77cf76be541e785ba80ed25896db492a76e602d1
Caspian Coders <61270465+caspiancoders@users.noreply.github.com>
2020-02-20 16:09:37 +0330

    add static keyword to defineOnce method

--- src/Util.php
+++ src/Util.php
@@ -4,7 +4,7 @@ namespace Caspian;
 
 class Util
 {
-    public function defineOnce($name, $value)
+    public static function defineOnce($name, $value)
     {
         if (!defined($name)) {
             define($name, $value);
